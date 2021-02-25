38f5c57a755f507f96472359ab0b9f62fd5b1da7
Zhijie Zheng <zhengz1@myumanitoba.ca>
2021-02-25 08:31:54 -0600

    Replace TodolistAdapter.java

--- app/src/main/java/com/groupeleven/studentlife/ui/todolist/TodolistAdapter.java
+++ app/src/main/java/com/groupeleven/studentlife/ui/todolist/TodolistAdapter.java
@@ -1,27 +1,26 @@
 package com.groupeleven.studentlife.ui.todolist;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.groupeleven.studentlife.R;
 import com.groupeleven.studentlife.domainSpecificObjects.Task;
-import com.groupeleven.studentlife.ui.dashboard.DashboardEventListAdapter;
+import com.groupeleven.studentlife.logic.TodolistLogic;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-
 public class TodolistAdapter extends RecyclerView.Adapter<TodolistAdapter.ViewHolder>{
     private Task[] taskList;
+    private int id;
+    private TodolistLogic logic;
 
     public TodolistAdapter(Task[] taskList){
         this.taskList = taskList;
@@ -44,70 +43,82 @@ public class TodolistAdapter extends RecyclerView.Adapter<TodolistAdapter.ViewHo
     public void onBindViewHolder(TodolistAdapter.ViewHolder holder, int position) {
         //get data according to position
         Task task = taskList[position];
-        holder.taskBox.setText(task.getTaskName());
+        id = position;
+        String output = task.getTaskName()+"\n"+
+                        task.getEndTime()+"\n"+
+                        logic.toPriority(task.getPriority());
+
+//--------------------------------------------------------------------------------------------------
+// show task detail to checkboxs
+        holder.taskBox.setText(output);
 
         holder.taskBox.setChecked(intToBoolean(task.getStatus()));
-
-        //holder.taskBox.;
-
-        /*
-        int status = task.getStatus();
-        SimpleDateFormat orgFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.CANADA);
-        SimpleDateFormat newFormat = new SimpleDateFormat("MMM dd", Locale.CANADA);
-        String statusText;
-        String endTime = "Parse Failed";
-
-        //populate information for the views
-        TextView taskNameView = holder.taskNameTextView;
-        taskNameView.setText(task.getTaskName());
-
-        TextView taskStatusView = holder.taskStatusTextView;
-        if(status < 5){
-            statusText = "HIGH";
-        }else if (status < 10){
-            statusText = "MEDIUM";
-        }else{
-            statusText = "LOW";
-        }
-        taskStatusView.setText(statusText);
-
-        TextView taskEndTimeView = holder.taskEndTimeTextView;
-        try {
-            endTime = newFormat.format(orgFormat.parse(task.getEndTime()));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(endTime);
-        taskEndTimeView.setText(endTime);*/
+//--------------------------------------------------------------------------------------------------
     }
 
+//--------------------------------------------------------------------------------------------------
+// 0 is false, 1 is true
     private boolean intToBoolean(int status){
         return status!=0;
    }
+//--------------------------------------------------------------------------------------------------
+
     @Override
     public int getItemCount() {
         return taskList.length;
     }
 
-    public void setTasks(Task[] taskList){
-        this.taskList = taskList;
-        notifyDataSetChanged();
+    public void fresh(){
+        this.notifyDataSetChanged();
     }
 
     // ViewHolder class provides refs to views (rows in RecyclerView)
     public class ViewHolder extends RecyclerView.ViewHolder{
 
         CheckBox taskBox;
+        FloatingActionButton edit;
+        FloatingActionButton delete;
+        TodolistLogic logic;
 
-        FloatingActionButton del;
         //itemView is the entire row in RecyclerView
         public ViewHolder(View itemView){
             super(itemView);
             //find the subviews
             taskBox = itemView.findViewById(R.id.todoCheckBox);
+            edit = itemView.findViewById(R.id.editTask);
+            delete = itemView.findViewById(R.id.deleteTask);
+            logic = new TodolistLogic();
+//--------------------------------------------------------------------------------------------------
+// "edit" button action, jump to edit activity
+            edit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent in = new Intent(itemView.getContext(), Toedit.class);
+                    in.putExtra(taskList[id].getTaskName(),id);
 
-            del = itemView.findViewById(R.id.deleteTask);
+                    itemView.getContext().startActivity(in);
+                }
+            });
+//--------------------------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------------------------
+//  "delete" button action, delete a task
+
+            delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(!logic.deleteTask(id)){
+                        Toast.makeText(itemView.getContext(),"Delete task fail",Toast.LENGTH_SHORT).show();
+
+                    }
+                    else{
+                        Toast.makeText(itemView.getContext(),"Task deleted task successfully",Toast.LENGTH_SHORT).show();
+                        notifyItemRemoved(id);
+
+                    }
+                }
+            });
+//--------------------------------------------------------------------------------------------------
         }
     }
 
