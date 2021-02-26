52814c74e28155679f0d5648f5e05d2a001b1858
Zhijie Zheng <zhengz1@myumanitoba.ca>
2021-02-26 05:47:38 -0600

    Replace Toadd.java

--- app/src/main/java/com/groupeleven/studentlife/ui/todolist/Toadd.java
+++ app/src/main/java/com/groupeleven/studentlife/ui/todolist/Toadd.java
@@ -2,7 +2,6 @@ package com.groupeleven.studentlife.ui.todolist;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputType;
@@ -19,8 +18,6 @@ import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.groupeleven.studentlife.R;
 import com.groupeleven.studentlife.logic.TodolistLogic;
