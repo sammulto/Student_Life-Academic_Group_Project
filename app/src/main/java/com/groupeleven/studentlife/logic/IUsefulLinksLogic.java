b378ea4130969b66ca840bd0eb0c9b0cd28e1414
Sammul To <55563166+sammulto@users.noreply.github.com>
2021-03-07 18:14:46 -0600

    Created DOS:Link And Useful Link Logic Interface

---
+++ app/src/main/java/com/groupeleven/studentlife/logic/IUsefulLinksLogic.java
@@ -0,0 +1,11 @@
+package com.groupeleven.studentlife.logic;
+
+import com.groupeleven.studentlife.domainSpecificObjects.Link;
+
+public interface IUsefulLinksLogic {
+
+    public Link[] getLinks( );
+
+    public int getSize();
+
+}
