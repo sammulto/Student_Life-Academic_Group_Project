8c578ba5d17821946b4c6d6dab237bcd2d76dfef
Muhammad Bilal Sheikh <sheikhmb@myumanitoba.ca>
2021-03-09 16:49:12 -0600

    Update InteractiveCalendarInterface.java

--- app/src/main/java/com/groupeleven/studentlife/ui/calendar/InteractiveCalendarInterface.java
+++ app/src/main/java/com/groupeleven/studentlife/ui/calendar/InteractiveCalendarInterface.java
@@ -4,13 +4,12 @@ import com.groupeleven.studentlife.domainSpecificObjects.Task;
 
 public interface InteractiveCalendarInterface {
     
-    public void viewTask(); //Tap on date to view Task
+    public void viewTasks(); //Tap on date to view Task
 
     public void editTask(); //Tap on edit to editTask (time/date/description)
 
     public void deleteTask(); //delete a task in the calendar
-
-    public void insertTask(); //insert a new task in the calendar (?)
-
-
+    
+    
+    
 }
