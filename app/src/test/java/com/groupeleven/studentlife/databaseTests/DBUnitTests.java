133731862717d644293502a3084482ffbe90993c
Sammul To <55563166+sammulto@users.noreply.github.com>
2021-03-09 14:01:47 -0600

    Restored Deleted files
    The files deleted from iteration 1 is restored:
    DBUnitTests.java
    TimeEstimatorUnitTest.java
    These tests are added to AllTests.java

---
+++ app/src/test/java/com/groupeleven/studentlife/databaseTests/DBUnitTests.java
@@ -0,0 +1,68 @@
+package com.groupeleven.studentlife.databaseTests;
+
+import org.junit.Test;
+import com.groupeleven.studentlife.domainSpecificObjects.Task;
+import com.groupeleven.studentlife.data.DB;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class DBUnitTests {
+
+    @Test
+    public void DBconstructor(){
+        DB db = new DB("ConstructorTest");
+        assertNotNull("Newly created DB object should not be null", db);
+        assertArrayEquals("Newly created DB object should be empty", db.getTasks(), new Task[0]);
+    }
+
+    @Test
+    public void insertBaseCase(){
+        DB db = new DB("InsertTest");
+        Task t = new Task("idk");
+        assertTrue("Insert should return true", db.insertTask(t));
+        Task[] tasks = new Task[1];
+        tasks[0] = t;
+        assertArrayEquals("The DB should only contain the task t", db.getTasks(), tasks);
+    }
+
+    @Test
+    public void insertWithDate(){
+        DB db = new DB("insertDateTest)");
+        Task t = new Task("idk");
+        t.setStartTime("2021-01-10 12:35:20");
+        t.setEndTime("2021-01-10 12:25:20");
+        assertTrue("Insert should return true", db.insertTask(t));
+        Task[] tasks = new Task[1];
+        tasks[0] = t;
+        assertArrayEquals("The DB should only contain the task t", db.getTasks(), tasks);
+    }
+
+    @Test
+    public void deleteBaseCase(){
+        DB db = new DB("DeleteTest");
+        Task t = new Task("idk");
+        db.insertTask(t);
+        assertTrue("Delete should return true", db.deleteTask(t));
+        assertArrayEquals("The database should be empty", db.getTasks(), new Task[0]);
+
+    }
+
+    @Test
+    public void updateBaseCase(){
+        DB db = new DB("UpdateTest");
+        Task t = new Task("idk");
+        Task[] tasks = new Task[1];
+        db.insertTask(t);
+        t.setType("Homework");
+        tasks[0] = t;
+        db.updateTask(t);
+        assertTrue("Update should return true", db.updateTask(t));
+        assertArrayEquals("The database should contain the updated task", db.getTasks(), tasks);
+    }
+
+}
