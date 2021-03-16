584dfef3e435824260f28233e241b9362d5f588b
AlFahiyanSiyam <siyamaf@myumanitoba.ca>
2021-03-16 04:01:01 -0400

    Calendar Tasklist UI updated

--- app/src/main/java/com/groupeleven/studentlife/ui/calendar/TaskListFragment.java
+++ app/src/main/java/com/groupeleven/studentlife/ui/calendar/TaskListFragment.java
@@ -1,3 +1,14 @@
+package com.groupeleven.studentlife.ui.calendar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.groupeleven.studentlife.R;
@@ -5,22 +16,19 @@ import com.groupeleven.studentlife.R;
 public class TaskListFragment extends Fragment {
 
 
-    public View onCreateView(LayoutInflater inflater, /*@Nullable*/ ViewGroup container, /*@Nullable*/ Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.fragment_a, container, false);
-        editText = v.findViewById(R.id.edit_text);
-        buttonOk = v.findViewById(R.id.button_ok);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_tasklist, container, false);
 
-        buttonOk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CharSequence input = editText.getText();
-                listener.onInputASent(input);
-            }
-        });
+
+
+
+
+
+
         return v;
     }
-
 
 
 }
