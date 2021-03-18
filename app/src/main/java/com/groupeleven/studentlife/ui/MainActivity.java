5d199f1cb37f260a6580ef574d093a44f944b17f
Trkun <39861323+Trkun@users.noreply.github.com>
2021-03-19 02:56:04 +0800

    clickable links done

--- app/src/main/java/com/groupeleven/studentlife/ui/MainActivity.java
+++ app/src/main/java/com/groupeleven/studentlife/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.groupeleven.studentlife.ui;
 
 import android.os.Bundle;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.groupeleven.studentlife.R;
 
@@ -27,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
+
     }
 
 }
