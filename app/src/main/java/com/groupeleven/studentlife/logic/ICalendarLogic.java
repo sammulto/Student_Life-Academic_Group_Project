52763d7bfabdf454cddd3e8388fc1a7f07e48915
Zhijie Zheng <zhengz1@myumanitoba.ca>
2021-03-19 14:23:06 -0500

    Replace ICalendarLogic.java

--- app/src/main/java/com/groupeleven/studentlife/logic/ICalendarLogic.java
+++ app/src/main/java/com/groupeleven/studentlife/logic/ICalendarLogic.java
@@ -8,13 +8,7 @@ public interface ICalendarLogic {
 
     public ITaskObject[] viewTask(String date) throws RuntimeException;//Tap on date to view Task
 
-    public boolean editTask(int id, String name, String priorityText, String endTime); //Tap on edit to editTask (time/date/description)
-
-    public boolean deleteTask(int id); //delete a task in the calendar
-
     public String getTaskPriorityText(ITaskObject task);
-
-    public boolean setCompleted(int id, boolean status);
 
     public int getTimeEstimate(int id, ITaskObject[] taskObjects);
 
