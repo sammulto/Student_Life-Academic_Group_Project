0317da62e15c83fa7f5ca25d6cbe395f4691dc17
Sammul To <55563166+sammulto@users.noreply.github.com>
2021-04-06 13:19:20 -0500

    updated tutorial acceptance tests

--- app/src/androidTest/java/com/groupeleven/studentlife/acceptanceTests/TutorialAcceptanceTests.java
+++ app/src/androidTest/java/com/groupeleven/studentlife/acceptanceTests/TutorialAcceptanceTests.java
@@ -1,10 +1,5 @@
 package com.groupeleven.studentlife.acceptanceTests;
 
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -12,9 +7,6 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import com.groupeleven.studentlife.R;
 import com.groupeleven.studentlife.ui.MainActivity;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
@@ -25,9 +17,8 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.Matchers.allOf;
+
 
 @RunWith(AndroidJUnit4.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
