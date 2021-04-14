f78e7b25ddd91ed0ea46148548c721e6be216721
Zhijie Zheng <zhengz1@myumanitoba.ca>
2021-04-14 15:17:11 -0500

    Replace TodoLogicAndDBTests.java

--- app/src/test/java/com/groupeleven/studentlife/IntegrationTests/TodoLogicAndDBTests.java
+++ app/src/test/java/com/groupeleven/studentlife/IntegrationTests/TodoLogicAndDBTests.java
@@ -40,8 +40,10 @@ public class TodoLogicAndDBTests {
 
     @Test
     public void getDataFromEmptyDBTest(){
-        ITaskObject list[] = logic.getData();
-        assertEquals("The size of the list should be zero.", 0, list.length);
+        ITaskObject completedList[] = logic.getUncompleted();
+        ITaskObject unCompletedList[] = logic.getCompleted();
+        assertEquals("The size of the completed list should be zero.", 0, completedList.length);
+        assertEquals("The size of the uncompleted list should be zero.", 0, unCompletedList.length);
     }
 
     @Test
@@ -52,20 +54,31 @@ public class TodoLogicAndDBTests {
         logic.addTask("Task 2","Medium","2020-01-01 12:12:12","2220-05-02 01:02:12","Reading",10,"Pages");
         logic.addTask("Task 3","Low","2020-01-01 12:12:12","2220-05-02 01:02:12","Reading",10,"Pages");
 
-        ITaskObject list[] = logic.getData();
-        assertEquals("The size of the list should be 3.", 3, list.length);
-        assertArrayEquals("The task list shoube be the same.",list,db.getTasks());
-        assertEquals("The Task name should be Task 1","Task 1",list[0].getTaskName());
+        ITaskObject unCompletedList[] = logic.getUncompleted();
+        ITaskObject completedList[] = logic.getCompleted();
+
+        assertEquals("The size of the unCompletedList should be 3.", 3, unCompletedList.length);
+        assertEquals("The size of the completedList should be 0.", 0, completedList.length);
+        assertArrayEquals("The task list should be the same.",unCompletedList,db.getTasks());
+        assertEquals("The Task name should be Task 1","Task 1",unCompletedList[0].getTaskName());
 
 
         //delete data from DB via Logic Unit
-        logic.deleteTask(1);
-        list = logic.getData();
-        assertEquals("The size of the list should be 2.", 2, list.length);
-        logic.deleteTask(0);
-        logic.deleteTask(0);
-        list = logic.getData();
-        assertEquals("The size of the list should be 0.", 0, list.length);
+        logic.deleteTask(false,1);
+        unCompletedList = logic.getUncompleted();
+        assertEquals("The size of the list should be 2.", 2, unCompletedList.length);
+        logic.deleteTask(false,0);
+
+        //delete a completed task from DB via Logic Unit
+        logic.setCompleted(false,0,true);
+        completedList = logic.getCompleted();
+        assertEquals("The size of the completedList should be 1.", 1, completedList.length);
+        logic.deleteTask(true,0);
+
+        completedList = logic.getCompleted();
+        unCompletedList = logic.getUncompleted();
+        assertEquals("The size of the completedList should be 0.", 0, completedList.length);
+        assertEquals("The size of the unCompletedList should be 0.", 0, unCompletedList.length);
     }
 
     @Test
@@ -74,18 +87,18 @@ public class TodoLogicAndDBTests {
         logic.addTask("Task 1", "High", "2020-01-01 12:12:12","2220-05-02 01:02:12","Reading",10,"pages");
         logic.addTask("Task 2", "High", "2020-01-01 12:12:12","2220-05-02 01:02:12","Reading",10,"pages");
         logic.addTask("Task 3", "High", "2020-01-01 12:12:12","2220-05-02 01:02:12","Reading",10,"pages");
-        ITaskObject[] listLogic = logic.getData();
+        ITaskObject[] listLogic = logic.getUncompleted();
 
         //Edit Task Name
         logic.editTask(1,"new name", "Low", "2020-01-01 12:12:12","2220-05-02 01:02:12","Reading",10,"pages");
-        String newTaskName = logic.getData()[1].getTaskName();
-        String newPriority = logic.getData()[1].getPriority().name();
+        String newTaskName = logic.getUncompleted()[1].getTaskName();
+        String newPriority = logic.getUncompleted()[1].getPriority().name();
         assertEquals("The updated Task name should be :","new name", newTaskName);
         assertEquals("The updated priority should be:LOW", "LOW", newPriority);
 
         logic.editTask(2,"another name", "Medium", "2020-01-01 12:12:12","2220-05-02 01:02:12","Reading",10,"pages");
-        String newTaskName2 = logic.getData()[2].getTaskName();
-        String newPriority2 = logic.getData()[2].getPriority().name();
+        String newTaskName2 = logic.getUncompleted()[2].getTaskName();
+        String newPriority2 = logic.getUncompleted()[2].getPriority().name();
         assertEquals("The updated Task name should be: another name","another name", newTaskName2);
         assertEquals("The updated priority should be:LOW", "MEDIUM", newPriority2);
     }
@@ -94,17 +107,16 @@ public class TodoLogicAndDBTests {
     @Test
     public void setCompletedTest(){
 
-
         logic.addTask("Task 1","High","2020-01-01 12:12:12","2220-05-02 01:02:12","Reading",10,"Pages");
 
         //set task as completed via Logic Unit
-        logic.setCompleted(0,true);
-        ITaskObject task = logic.getData()[0];
+        logic.setCompleted(false,0,true);
+        ITaskObject task = logic.getCompleted()[0];
         assertEquals("Task.isComplete should be true:",true, task.isCompleted());
 
         //set task as uncompleted via Logic Unit
-        logic.setCompleted(0,false);
-        task = logic.getData()[0];
+        logic.setCompleted(true,0,false);
+        task = logic.getUncompleted()[0];
         assertEquals("Task.isComplete should be false:",false, task.isCompleted());
     }
 
