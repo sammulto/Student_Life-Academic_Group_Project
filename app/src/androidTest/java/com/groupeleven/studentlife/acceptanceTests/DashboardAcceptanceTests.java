50deb974139822e62199d0cf98db426ce7bc2042
Zhijie Zheng <zhengz1@myumanitoba.ca>
2021-04-15 20:03:59 -0500

    Replace DashboardAcceptanceTests.java

--- app/src/androidTest/java/com/groupeleven/studentlife/acceptanceTests/DashboardAcceptanceTests.java
+++ app/src/androidTest/java/com/groupeleven/studentlife/acceptanceTests/DashboardAcceptanceTests.java
@@ -1,7 +1,5 @@
 package com.groupeleven.studentlife.acceptanceTests;
 
-import androidx.test.espresso.ViewInteraction;
-import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -23,17 +21,12 @@ import org.junit.runners.MethodSorters;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isNotChecked;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static com.schibsted.spain.barista.assertion.BaristaListAssertions.assertCustomAssertionAtPosition;
 import static com.schibsted.spain.barista.assertion.BaristaListAssertions.assertListItemCount;
-import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.not;
 
 
 @RunWith(AndroidJUnit4.class)
